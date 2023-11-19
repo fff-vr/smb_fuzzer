65c4cd31883de32e72c5a7e1b48a655a1dbde6ca
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-20 00:07:50 +0900

    add mutator thread for manager

--- host_manager/src/main.rs
+++ host_manager/src/main.rs
@@ -39,6 +39,40 @@ fn connect_to_server(addr: &str) -> io::Result<()> {
     Ok(())
 }
 
+
+fn connect_and_write_to_server(addr: &str) -> io::Result<()> {
+    match TcpStream::connect(addr) {
+        Ok(mut stream) => {
+            println!("Client connected to {}", addr);
+
+            loop {
+                let message = b"ABCD1234"; // 서버로 보낼 메시지
+
+                match stream.write_all(message) {
+                    Ok(_) => {
+                        println!("Message sent to server on {}", addr);
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to write to server: {}", e);
+                        break;
+                    }
+                }
+
+                // 일정 시간 대기 후 메시지 재전송 (예: 5초)
+                thread::sleep(Duration::from_secs(5));
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to connect to server: {}. Retrying in 10 seconds...", e);
+            thread::sleep(Duration::from_secs(10));
+            connect_and_write_to_server(addr); // 재시도
+        }
+    }
+
+    Ok(())
+}
+
+
 fn main() -> io::Result<()> {
     let ip_address = "127.0.0.1";
     let start_port = 10023;
@@ -49,6 +83,17 @@ fn main() -> io::Result<()> {
 
         thread::spawn(move || {
             if let Err(e) = connect_to_server(&addr) {
+                eprintln!("An error occurred: {}", e);
+            }
+        });
+    }
+    let start_port = 10022;
+    for i in 0..12 {
+        let port = start_port + i * 3;
+        let addr = format!("{}:{}", ip_address, port);
+
+        thread::spawn(move || {
+            if let Err(e) = connect_and_write_to_server(&addr) {
                 eprintln!("An error occurred: {}", e);
             }
         });
