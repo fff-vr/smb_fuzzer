e1a9dc6ae80ba570d267883ec2c517bddb634b61
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-19 22:33:04 +0900

    simple mount check

--- guest_user_agent/guest_user_agent.c
+++ guest_user_agent/guest_user_agent.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/types.h>
-
+#include <sys/mount.h>
 #define KCOV_INIT_TRACE                     _IOR('c', 1, unsigned long)
 #define KCOV_ENABLE                 _IO('c', 100)
 #define KCOV_DISABLE                        _IO('c', 101)
@@ -18,6 +18,20 @@
 #define KCOV_TRACE_PC  0
 #define KCOV_TRACE_CMP 1
 
+
+void mount_cifs(){
+
+    const char* source = "//127.0.0.1/share"; // SMB 공유 경로
+	const char* target = "/mnt/"; // 마운트 포인트
+	const char* filesystemtype = "cifs";
+	unsigned long mountflags = MS_MGC_VAL;
+	const char* data = "username=your_username,password=your_password,vers=1.0"; // 사용자 이름과 비밀번호
+    if (mount(source, target, filesystemtype, mountflags, data) != 0) {
+        //fprintf(stderr, "Error mounting cifs filesystem: %s\n", strerror(errno));
+        //TODO if refuse -> retry
+        return -1;
+    }
+}
 int main(int argc, char **argv)
 {
     int fd;
@@ -43,7 +57,9 @@ int main(int argc, char **argv)
     /* Reset coverage from the tail of the ioctl() call. */
     __atomic_store_n(&cover[0], 0, __ATOMIC_RELAXED);
     /* Call the target syscall call. */
-    read(-1, NULL, 0);
+
+    mount_cifs();
+
     /* Read number of PCs collected. */
     n = __atomic_load_n(&cover[0], __ATOMIC_RELAXED);
     for (i = 0; i < n; i++)
