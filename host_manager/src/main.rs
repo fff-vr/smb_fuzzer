4e20727ee74bd5e37a04d788cd58d29b6a3f5fd3
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-19 22:58:38 +0900

    init manager

---
+++ host_manager/src/main.rs
@@ -0,0 +1,19 @@
+use std::net::TcpStream;
+use std::io::{self, Read};
+use byteorder::{ReadBytesExt, BigEndian};
+
+fn main() -> io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:10023")?;
+
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer)?;
+
+    let mut cursor = io::Cursor::new(buffer);
+
+    while let Ok(value) = cursor.read_u64::<BigEndian>() {
+        println!("Received value: 0x{:X}", value);
+    }
+
+    Ok(())
+}
+
