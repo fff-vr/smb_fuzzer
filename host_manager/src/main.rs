d7556216cb14e4f40414fc7e889ed6c328146db0
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-19 23:08:20 +0900

    skeleton of master

--- host_manager/src/main.rs
+++ host_manager/src/main.rs
@@ -1,19 +1,67 @@
 use std::net::TcpStream;
-use std::io::{self, Read};
-use byteorder::{ReadBytesExt, LittleEndian};
+use std::io::{self, Read, Write};
+use std::thread;
+use std::time::Duration;
 
-fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:10023")?;
+fn connect_to_server(addr: &str) -> io::Result<()> {
+    loop {
+        match TcpStream::connect(addr) {
+            Ok(mut stream) => {
+                println!("Client connected to {}", addr);
 
-    let mut buffer = Vec::new();
-    stream.read_to_end(&mut buffer)?;
+                // 서버와의 통신 로직을 여기에 추가합니다.
 
-    let mut cursor = io::Cursor::new(buffer);
+                // 예시: 서버에 메시지를 보내기
+                let message = format!("Hello from client on port {}", addr);
+                if let Err(e) = stream.write_all(message.as_bytes()) {
+                    eprintln!("Failed to send message from client on port {}: {}", addr, e);
+                    continue;
+                }
 
-    while let Ok(value) = cursor.read_u64::<LittleEndian>() {
-        println!("Received value: 0x{:X}", value);
+                // 예시: 서버로부터 데이터 읽기
+                let mut buffer = [0; 8 *0x100000];
+                match stream.read(&mut buffer) {
+                    Ok(_) => {
+                        let received = String::from_utf8_lossy(&buffer);
+                        println!("Client on port {} received: {}", addr, received);
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read from server for client on port {}: {}", addr, e);
+                    }
+                }
+
+                // 연결 및 통신이 성공적으로 완료되면 루프를 종료합니다.
+                break;
+            }
+            Err(e) => {
+                eprintln!("Failed to connect for client on port {}: {}. Retrying in 10 seconds...", addr, e);
+                // 10초 동안 대기 후 재시도
+                thread::sleep(Duration::from_secs(10));
+            }
+        }
     }
 
     Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let ip_address = "127.0.0.1";
+    let start_port = 10023; // 시작 포트 번호
+
+    for i in 0..12 {
+        let port = start_port + i * 3; // 포트 번호 계산
+        let addr = format!("{}:{}", ip_address, port);
+
+        thread::spawn(move || {
+            if let Err(e) = connect_to_server(&addr) {
+                eprintln!("An error occurred: {}", e);
+            }
+        });
+    }
+
+    // 메인 스레드를 무한 루프 상태로 유지합니다.
+    loop {
+        thread::sleep(Duration::from_secs(60));
+    }
 }
 
