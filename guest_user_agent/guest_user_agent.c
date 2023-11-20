f0745480c33cba7ebe56ee004f437bc35fa051e6
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-20 19:32:34 +0900

    print retry num

--- guest_user_agent/guest_user_agent.c
+++ guest_user_agent/guest_user_agent.c
@@ -120,8 +120,9 @@ int main(int argc, char **argv)
         int ret =0;
         while(ret != 1){
             ret = read(master,buffer,1);
+            int retry =0;
             if(ret!=1){
-                printf("wait for recv command from Master\n");
+                printf("wait for recv command from Master. %d\n",retry++);
                 sleep(1);
             }
         }
