1e10c6b76537c92d5c67930904736a93cebe0504
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-20 20:56:13 +0900

    change proxt 2 listen

--- proxy_for_mutator/proxy.c
+++ proxy_for_mutator/proxy.c
@@ -3,118 +3,136 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT1 8080
 #define PORT2 8082
-#define BUFFER_SIZE 1024 * 20
+#define BUFFER_SIZE 1024
 
-char buffer[BUFFER_SIZE] = {0};
+int client_socket_8082 = -1; // Socket for the client on port 8082
+pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *handle_relay(void *arg);
+void *handle_client_8082(void *arg);
+int setup_server_socket(int port);
+void send_to_client_8082(char *msg, int len);
+
 int main() {
-    int server_fd, new_socket, sending_socket;
-    struct sockaddr_in server_addr, client_addr, dest_addr;
-    int opt = 1;
-    int addrlen = sizeof(server_addr);
+    int server_fd1, server_fd2;
+    pthread_t relay_thread, client_thread_8082;
 
-    // Creating socket file descriptor
+    // Setup server sockets
+    server_fd1 = setup_server_socket(PORT1);
+    server_fd2 = setup_server_socket(PORT2);
+
+    // Start a thread to handle the relay of data from port 8080
+    if (pthread_create(&relay_thread, NULL, handle_relay, (void *)(intptr_t)server_fd1) != 0) {
+        perror("Failed to create relay thread");
+        return 1;
+    }
+
+    // Start a thread to handle a single client connection on port 8082
+    if (pthread_create(&client_thread_8082, NULL, handle_client_8082, (void *)(intptr_t)server_fd2) != 0) {
+        perror("Failed to create client thread for port 8082");
+        return 1;
+    }
+
+    pthread_join(relay_thread, NULL);
+    pthread_join(client_thread_8082, NULL);
+
+    return 0;
+}
+
+int setup_server_socket(int port) {
+    int server_fd, opt = 1;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Create socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
+    // Attach socket to the port
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT1);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
 
-    // Bind the socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    // Bind the socket
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Listen for incoming connections
+    // Listen
     if (listen(server_fd, 3) < 0) {
         perror("Listen");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        printf("Waiting for new connection...\n");
+    return server_fd;
+}
 
-        // Accept a connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen)) < 0) {
-            perror("Accept");
-            continue; // Continue to next iteration on error
-        }
-
-        ssize_t total_bytes_read = 0;
-        ssize_t bytes_read;
-        while ((bytes_read = read(new_socket, buffer + total_bytes_read, BUFFER_SIZE - total_bytes_read)) > 0) {
-            total_bytes_read += bytes_read;
-            if (total_bytes_read >= BUFFER_SIZE) break;
-        }
-
-        if (bytes_read < 0) {
-            perror("Read error");
-            close(new_socket);
-            continue;
-        }
-
-        printf("Received data: %s\n", buffer);
-
-        // Creating a socket for sending data
-        if ((sending_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            perror("Socket creation error");
-            close(new_socket);
-            continue; // Continue to next iteration on error
-        }
-
-        dest_addr.sin_family = AF_INET;
-        dest_addr.sin_port = htons(PORT2);
-
-        // Convert IPv4 addresses from text to binary form
-        if (inet_pton(AF_INET, "127.0.0.1", &dest_addr.sin_addr) <= 0) {
-            perror("Invalid address/ Address not supported");
-            close(sending_socket);
-            close(new_socket);
-            continue; // Continue to next iteration on error
-        }
-
-        // Connect to the server on port 8082
-        if (connect(sending_socket, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
-            perror("Connection Failed");
-            close(sending_socket);
-            close(new_socket);
-            continue; // Continue to next iteration on error
-        }
-
-        ssize_t total_bytes_sent = 0;
-        ssize_t bytes_sent;
-        while (total_bytes_sent < total_bytes_read) {
-            bytes_sent = send(sending_socket, buffer + total_bytes_sent, total_bytes_read - total_bytes_sent, 0);
-            if (bytes_sent < 0) {
-                perror("Send error");
-                break;
-            }
-            total_bytes_sent += bytes_sent;
-        }
-
-        if (bytes_sent >= 0) {
-            printf("Data forwarded to port 8082\n");
-        }
-
-        close(new_socket);
-        close(sending_socket);
+void *handle_relay(void *arg) {
+    int server_fd = (intptr_t)arg;
+    int new_socket = accept(server_fd, NULL, NULL);
+    if (new_socket < 0) {
+        perror("Accept failed");
+        exit(EXIT_FAILURE);
     }
 
-    // The following line will never be reached in this example
-    // close(server_fd);
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        ssize_t bytes_read = read(new_socket, buffer, BUFFER_SIZE);
+        if (bytes_read <= 0) {
+            perror("Read failed");
+            break;
+        }
 
-    return 0;
+        pthread_mutex_lock(&client_mutex);
+        send_to_client_8082(buffer, bytes_read);
+        pthread_mutex_unlock(&client_mutex);
+    }
+
+    close(new_socket);
+    return NULL;
+}
+
+void *handle_client_8082(void *arg) {
+    int server_fd = (intptr_t)arg;
+    while (1) {
+        if (client_socket_8082 == -1) { // Check if no client is connected
+            int new_socket = accept(server_fd, NULL, NULL);
+            if (new_socket < 0) {
+                perror("Accept failed");
+                continue;
+            }
+
+            pthread_mutex_lock(&client_mutex);
+            client_socket_8082 = new_socket; // Update the client socket
+            pthread_mutex_unlock(&client_mutex);
+        }
+
+        // Wait for the client to disconnect
+        char buffer[BUFFER_SIZE];
+        if (recv(client_socket_8082, buffer, BUFFER_SIZE, 0) <= 0) {
+            close(client_socket_8082);
+            client_socket_8082 = -1; // Reset the client socket
+        }
+    }
+
+    return NULL;
+}
+
+void send_to_client_8082(char *msg, int len) {
+    if (client_socket_8082 != -1) {
+        send(client_socket_8082, msg, len, 0);
+    }
 }
 
