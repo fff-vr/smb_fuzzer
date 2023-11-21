391e1d90a80351abdd052f07bb1fad749ce9061b
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-21 23:18:48 +0900

    add debug dump at mutate

--- host_manager/src/mutate.rs
+++ host_manager/src/mutate.rs
@@ -1,16 +1,29 @@
 use rand::Rng;
-
+use debug_print::{debug_print, debug_println, debug_eprint, debug_eprintln};
 /// Mutates a part of the given Vec<u8>.
 /// 
 /// # Arguments
 ///
 /// * `data` - A mutable reference to a Vec<u8> to mutate.
 /// * `mutation_rate` - A percentage (0-100) indicating how much of the Vec should be mutated.
+fn hexdump(label: &str, data: &[u8]) {
+    debug_println!("{}", label);
+    for (i, byte) in data.iter().enumerate() {
+        print!("{:02x} ", byte);
+        if (i + 1) % 16 == 0 {
+            println!();
+        }
+    }
+    debug_println!("\n");
+}
+
+
 pub fn mutate(data: &mut Vec<u8>, mutation_rate: f32) {
     let length = data.len();
     if length <4{
         return;
     }
+    hexdump("before mutate",&data[0..]);
     let num_mutations = (length as f32 * mutation_rate / 100.0).round() as usize;
 
     let mut rng = rand::thread_rng();
@@ -20,5 +33,7 @@ pub fn mutate(data: &mut Vec<u8>, mutation_rate: f32) {
         // but other mutations like random byte replacement can also be used.
         data[index] = rng.gen();
     }
+
+    hexdump("after mutate",&data[0..]);
 }
 
