76cebd06821a930c1fb00908e1d6f03620a8c968
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-21 23:15:27 +0900

    improbe mutate

--- host_manager/src/mutate.rs
+++ host_manager/src/mutate.rs
@@ -8,14 +8,17 @@ use rand::Rng;
 /// * `mutation_rate` - A percentage (0-100) indicating how much of the Vec should be mutated.
 pub fn mutate(data: &mut Vec<u8>, mutation_rate: f32) {
     let length = data.len();
+    if length <4{
+        return;
+    }
     let num_mutations = (length as f32 * mutation_rate / 100.0).round() as usize;
 
     let mut rng = rand::thread_rng();
     for _ in 0..num_mutations {
-        let index = rng.gen_range(0..length);
+        let index = rng.gen_range(4..length);
         // Mutate the byte at the chosen index. This example simply toggles the byte,
         // but other mutations like random byte replacement can also be used.
-        data[index] = !data[index];
+        data[index] = rng.gen();
     }
 }
 
