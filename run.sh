25b09ce0471acfc083e16b1beae6eefa906481f3
jjy <wiwiwi79@hanyang.ac.kr>
2023-11-22 21:02:45 +0900

    add vm

---
+++ run.sh
@@ -0,0 +1,12 @@
+KERNEL=/home/jjy/target/linux/
+IMAGE=./tools
+qemu-system-x86_64 \
+	-m 16G \
+	-smp 12,sockets=12,cores=1 \
+	-kernel $KERNEL/arch/x86/boot/bzImage \
+	-append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0" \
+	-hda ./bullseye.img \
+	-net user,host=10.0.2.10,hostfwd=tcp:0.0.0.0:10021-:22,hostfwd=tcp:0.0.0.0:10022-:8080,hostfwd=tcp:0.0.0.0:10023-:8081 \
+	-net nic,model=e1000 \
+	-enable-kvm \
+	-nographic
